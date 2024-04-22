4b0a333e8e75edda12fa9a519d1e44e286e502ef
pav3433 <gubkin.14@yandex.ru>
2024-04-22 14:16:12 +0300

    change4

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,24 +1,37 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImp implements CarService {
 
-    private CarDao carDao;
+    private List<Car> carList;
 
-    @Autowired
-    public CarServiceImp(CarDao carDao) {
-        this.carDao = carDao;
+    public CarServiceImp() {
+        carList = new ArrayList<>();
+        carList.add(new Car("BMW", 7, 5000));
+        carList.add(new Car("Lada", 2107, 700));
+        carList.add(new Car("Toyota", 300, 8000));
+        carList.add(new Car("Porsche", 911, 12000));
+        carList.add(new Car("Hyundai", 30, 1500));
+        carList.add(new Car("BMW", 7, 5000));
+        carList.add(new Car("Lada", 2107, 700));
+        carList.add(new Car("Toyota", 300, 8000));
+        carList.add(new Car("Porsche", 911, 12000));
+        carList.add(new Car("Hyundai", 30, 1500));
     }
 
     @Override
     public List<Car> getCars(int count) {
-        return carDao.getCars(count);
+        if (count >= 5) {
+            return carList;
+        } else {
+            return carList.stream().limit(count).collect(Collectors.toList());
+        }
     }
 }
